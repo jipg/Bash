dd8dd752667407c10e0ff11f643aecf56c12865e
Jorge Ivan Posada <jiposada@oc1767731732.ibm.com>
2019-05-30 05:34:20 -0500

    Se añade book_el_shell_bash 1.2

--- book_el_shell_bash/tema_1_1_shell.sh
+++ book_el_shell_bash/tema_1_1_shell.sh
@@ -1,3 +1,47 @@
+#*************************************************
+# Autor: Jorge Iván Posada Gámez
+# fecha ddmmyy: 29052019
+# comentarios:
+#   - Ejercicios del libro el shell de bash
+#   - TEMA 1
+#     1. El shell que estamos usando
+#
+#*************************************************
+
+
+echo ""
+echo "#########################################"
+echo "scripts tema 1.1. libro el shell de bash"
+echo "_________________________________________"
+echo ""
+
+# La shell estamos usando y en que directorio está instalada.
 echo "La shell que se está usando:"
 echo "$SHELL"
+echo "---------------------------"
+echo ""
 
+# La version de bash.
+echo "La version de la shell de bash:"
+echo "$BASH_VERSION"
+echo "---------------------------"
+echo ""
+
+# Ruta donde está instalado bash.
+echo "Ruta donde está instalado bash:"
+whereis bash
+echo "---------------------------"
+echo ""
+
+# Todos los shells disponibles en la máquina.
+echo "Todos los shells disponibles en la máquina:"
+cat /etc/shells
+echo "---------------------------"
+echo ""
+
+# chsh : comando para cambiar la shell por defecto que se está usando.
+# chsh -s /bin/bash  #Coloca por defecto el shell bash
+echo "Coloca por defecto el shell bash:"
+echo "chsh -s /bin/bash"
+echo "---------------------------"
+echo ""
---
+++ book_el_shell_bash/tema_1_2_expansion.sh
@@ -0,0 +1,27 @@
+#*************************************************
+# Autor: Jorge Iván Posada Gámez
+# fecha ddmmyy: 29052019
+# comentarios:
+#   - Ejercicios del libro el shell de bash
+#   - TEMA 1
+#     2. Expansión de nombres de ficheros y directorios
+#
+#*************************************************
+
+# Comodines :
+# - comodín     ?               : Uno y sólo un caracter.
+# - comodín     *               : Cero o más caracteres.
+# - comodín     [conjunto]      : Uno de los caracteres que está en el conjunto.
+# - comodín     ![conjunto]     : Uno de los caracteres que está en el conjunto.
+# - comodín     ~               : Hace referencia al directorio home de los usuarios.
+# - comodín     ~/              : Hace referencia al directorio propio.
+# - comodín     {contenido}     : Hace una expansión según el contenido que se encuentra entre llaves.
+
+
+
+
+
+
+
+
+
